ed34bbb8905e86568a45ccd2af50b0201c773ebd
Eric Soros <ericsoros@Erics-MacBook-Pro-2.local>
2023-06-15 08:58:41 -0400

    adding animation:

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,19 @@ function App() {
   function calculate() {
     let amount_num = parseInt(amount)
     if(Number.isNaN(amount_num)) {
-      setErr("Unable to parse amount")
+      setErr("please input starting amount")
       return
     }
     
     let interest_amount = parseFloat(interestAmount) * 0.01
     if(Number.isNaN(interest_amount)) {
-      setErr("Unable to interest amount")
+      setErr("please input interest amount")
       return
     }
 
     let tradeDays_num = parseInt(tradeDays)
     if(Number.isNaN(tradeDays_num)) {
-      setErr("Unable to trade days")
+      setErr("please input trade days")
       return
     }
 
@@ -35,8 +35,8 @@ function App() {
   }
 
   return <div style={{display: "flex", flexDirection: "column", width: "50vw"}}>
-    <span className="material-symbols-outlined" style={{fontSize: "10rem", color: "orange"}}>
-      nutrition
+    <span className="material-symbols-outlined hover-animated" style={{fontSize: "10rem", color: "green", cursor: "default"}}>
+      universal_currency_alt
     </span>
 
     <label>Trade Days</label>
@@ -48,11 +48,11 @@ function App() {
     <label>Interest (Percent)</label>
     <input value={interestAmount} onChange={(e) => setInterestAmount(e.currentTarget.value)}></input>
 
-    {
-      err ? <p>err</p> : <></>
-    }
-
     <button onClick={calculate}>Calculate</button>
+    {
+      err ? <p>{err}</p> : <></>
+    }
+    
     {
       amounts == undefined ? <></> : 
       <div style={{display: "flex", flexDirection: "column", height: "50vh", overflowY: "scroll"}}>
